28cbae854a32980076106c32f93c314af8405fa7
chanxuehong <chanxuehong@gmail.com>
2014-05-25 19:56:21 +0800

    no message

--- server.go
+++ server.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"github.com/chanxuehong/util/pool"
 	"github.com/chanxuehong/wechat/message"
+	"io"
 	"io/ioutil"
 	"net/http"
 )
@@ -81,105 +82,144 @@ func NewServer(token string, requestPoolSize int) *Server {
 
 // Server 实现 http.Handler 接口
 func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	var err error
-	var signature, timestamp, nonce string
+	switch r.Method {
 
-	if err = r.ParseForm(); err != nil {
-		s.invalidRequestHandler(w, r, err)
-		return
-	}
+	// 处理从微信服务器推送过来的消息 ==============================================
+	case "POST":
+		var err error
+		var signature, timestamp, nonce string
 
-	if signature = r.FormValue("signature"); signature == "" {
-		s.invalidRequestHandler(w, r, errors.New("signature is empty"))
-		return
-	}
-	if timestamp = r.FormValue("timestamp"); timestamp == "" {
-		s.invalidRequestHandler(w, r, errors.New("timestamp is empty"))
-		return
-	}
-	if nonce = r.FormValue("nonce"); nonce == "" {
-		s.invalidRequestHandler(w, r, errors.New("nonce is empty"))
-		return
-	}
-
-	if !CheckSignature(signature, timestamp, nonce, s.token) {
-		s.invalidRequestHandler(w, r, errors.New("check signature failed"))
-		return
-	}
-
-	b, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		s.invalidRequestHandler(w, r, err)
-		return
-	}
-
-	rqstMsg := s.getRequestEntity()   // *message.Request
-	defer s.putRequestEntity(rqstMsg) // important!
-
-	if err = xml.Unmarshal(b, rqstMsg); err != nil {
-		s.invalidRequestHandler(w, r, err)
-		return
-	}
-
-	// request router
-	switch rqstMsg.MsgType {
-
-	case message.RQST_MSG_TYPE_TEXT:
-		s.textRequestHandler(w, r, rqstMsg)
-
-	case message.RQST_MSG_TYPE_VOICE:
-		if rqstMsg.Recognition == "" { // 普通的语音请求
-			s.voiceRequestHandler(w, r, rqstMsg)
-		} else { // 语音识别请求
-			s.voiceRecognitionRequestHandler(w, r, rqstMsg)
+		if err = r.ParseForm(); err != nil {
+			s.invalidRequestHandler(w, r, err)
+			return
 		}
 
-	case message.RQST_MSG_TYPE_LOCATION:
-		s.locationRequestHandler(w, r, rqstMsg)
+		if signature = r.FormValue("signature"); signature == "" {
+			s.invalidRequestHandler(w, r, errors.New("signature is empty"))
+			return
+		}
+		if timestamp = r.FormValue("timestamp"); timestamp == "" {
+			s.invalidRequestHandler(w, r, errors.New("timestamp is empty"))
+			return
+		}
+		if nonce = r.FormValue("nonce"); nonce == "" {
+			s.invalidRequestHandler(w, r, errors.New("nonce is empty"))
+			return
+		}
 
-	case message.RQST_MSG_TYPE_LINK:
-		s.linkRequestHandler(w, r, rqstMsg)
+		if !CheckSignature(signature, timestamp, nonce, s.token) {
+			s.invalidRequestHandler(w, r, errors.New("check signature failed"))
+			return
+		}
 
-	case message.RQST_MSG_TYPE_IMAGE:
-		s.imageRequestHandler(w, r, rqstMsg)
+		b, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			s.invalidRequestHandler(w, r, err)
+			return
+		}
 
-	case message.RQST_MSG_TYPE_VIDEO:
-		s.videoRequestHandler(w, r, rqstMsg)
+		rqstMsg := s.getRequestEntity()   // *message.Request
+		defer s.putRequestEntity(rqstMsg) // important!
 
-	case message.RQST_MSG_TYPE_EVENT:
-		// event router
-		switch rqstMsg.Event {
+		if err = xml.Unmarshal(b, rqstMsg); err != nil {
+			s.invalidRequestHandler(w, r, err)
+			return
+		}
 
-		case message.RQST_EVENT_TYPE_SUBSCRIBE:
-			if rqstMsg.Ticket == "" {
-				s.subscribeEventRequestHandler(w, r, rqstMsg)
-			} else { // 扫描二维码订阅
-				s.subscribeEventByScanRequestHandler(w, r, rqstMsg)
+		// request router
+		switch rqstMsg.MsgType {
+
+		case message.RQST_MSG_TYPE_TEXT:
+			s.textRequestHandler(w, r, rqstMsg)
+
+		case message.RQST_MSG_TYPE_VOICE:
+			if rqstMsg.Recognition == "" { // 普通的语音请求
+				s.voiceRequestHandler(w, r, rqstMsg)
+			} else { // 语音识别请求
+				s.voiceRecognitionRequestHandler(w, r, rqstMsg)
 			}
 
-		case message.RQST_EVENT_TYPE_UNSUBSCRIBE:
-			s.unsubscribeEventRequestHandler(w, r, rqstMsg)
+		case message.RQST_MSG_TYPE_LOCATION:
+			s.locationRequestHandler(w, r, rqstMsg)
 
-		case message.RQST_EVENT_TYPE_SCAN:
-			s.scanEventRequestHandler(w, r, rqstMsg)
+		case message.RQST_MSG_TYPE_LINK:
+			s.linkRequestHandler(w, r, rqstMsg)
 
-		case message.RQST_EVENT_TYPE_LOCATION:
-			s.locationEventRequestHandler(w, r, rqstMsg)
+		case message.RQST_MSG_TYPE_IMAGE:
+			s.imageRequestHandler(w, r, rqstMsg)
 
-		case message.RQST_EVENT_TYPE_CLICK:
-			s.clickEventRequestHandler(w, r, rqstMsg)
+		case message.RQST_MSG_TYPE_VIDEO:
+			s.videoRequestHandler(w, r, rqstMsg)
 
-		case message.RQST_EVENT_TYPE_VIEW:
-			s.viewEventRequestHandler(w, r, rqstMsg)
+		case message.RQST_MSG_TYPE_EVENT:
+			// event router
+			switch rqstMsg.Event {
 
-		case message.RQST_EVENT_TYPE_MASSSENDJOBFINISH:
-			s.masssendjobfinishEventRequestHandler(w, r, rqstMsg)
+			case message.RQST_EVENT_TYPE_SUBSCRIBE:
+				if rqstMsg.Ticket == "" {
+					s.subscribeEventRequestHandler(w, r, rqstMsg)
+				} else { // 扫描二维码订阅
+					s.subscribeEventByScanRequestHandler(w, r, rqstMsg)
+				}
 
-		default: // unknown event
+			case message.RQST_EVENT_TYPE_UNSUBSCRIBE:
+				s.unsubscribeEventRequestHandler(w, r, rqstMsg)
+
+			case message.RQST_EVENT_TYPE_SCAN:
+				s.scanEventRequestHandler(w, r, rqstMsg)
+
+			case message.RQST_EVENT_TYPE_LOCATION:
+				s.locationEventRequestHandler(w, r, rqstMsg)
+
+			case message.RQST_EVENT_TYPE_CLICK:
+				s.clickEventRequestHandler(w, r, rqstMsg)
+
+			case message.RQST_EVENT_TYPE_VIEW:
+				s.viewEventRequestHandler(w, r, rqstMsg)
+
+			case message.RQST_EVENT_TYPE_MASSSENDJOBFINISH:
+				s.masssendjobfinishEventRequestHandler(w, r, rqstMsg)
+
+			default: // unknown event
+				s.unknownRequestHandler(w, r, rqstMsg)
+			}
+
+		default: // unknown request message type
 			s.unknownRequestHandler(w, r, rqstMsg)
 		}
 
-	default: // unknown request message type
-		s.unknownRequestHandler(w, r, rqstMsg)
+	// 首次验证 =================================================================
+	case "GET":
+		var err error
+		var signature, timestamp, nonce, echostr string
+
+		if err = r.ParseForm(); err != nil {
+			s.invalidRequestHandler(w, r, err)
+			return
+		}
+
+		if signature = r.FormValue("signature"); signature == "" {
+			s.invalidRequestHandler(w, r, errors.New("signature is empty"))
+			return
+		}
+		if timestamp = r.FormValue("timestamp"); timestamp == "" {
+			s.invalidRequestHandler(w, r, errors.New("timestamp is empty"))
+			return
+		}
+		if nonce = r.FormValue("nonce"); nonce == "" {
+			s.invalidRequestHandler(w, r, errors.New("nonce is empty"))
+			return
+		}
+		if echostr = r.FormValue("echostr"); echostr == "" {
+			s.invalidRequestHandler(w, r, errors.New("echostr is empty"))
+			return
+		}
+
+		if !CheckSignature(signature, timestamp, nonce, s.token) {
+			s.invalidRequestHandler(w, r, errors.New("check signature failed"))
+			return
+		}
+
+		io.WriteString(w, echostr)
 	}
 }
