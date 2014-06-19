a32ce1faf2e6ea29ef0edf6cc48e139b510757eb
chanxuehong <chanxuehong@gmail.com>
2014-06-19 23:20:31 +0800

    修改 CheckSignature 里的比较方式，常数时间比较！

--- signature.go
+++ signature.go
@@ -2,17 +2,22 @@ package wechat
 
 import (
 	"crypto/sha1"
+	"crypto/subtle"
 	"encoding/hex"
 	"sort"
 )
 
 // 校验消息是否是从微信服务器发送过来的
 func CheckSignature(signature, timestamp, nonce, token string) bool {
+	if len(signature) != 40 {
+		return false
+	}
+
 	strArr := sort.StringSlice{token, timestamp, nonce}
 	strArr.Sort()
 
 	// buf := []byte(strArr[0] + strArr[1] + strArr[2])
-	// 因为目前 golang 还不支持 copy-on-write, 为了减少内存复制采用了变通的方法
+	// 因为目前 golang 还不支持 copy-on-write, 为了减少内存复制采用了这个的方法
 	str0Len := len(strArr[0])
 	str0str1Len := str0Len + len(strArr[1])
 	str0str1str2Len := str0str1Len + len(strArr[2])
@@ -21,12 +26,13 @@ func CheckSignature(signature, timestamp, nonce, token string) bool {
 	copy(buf[str0Len:], strArr[1])
 	copy(buf[str0str1Len:], strArr[2])
 
-	sha1SumBytes := sha1.Sum(buf) // 要求 go1.2+
-	sha1SumStr := hex.EncodeToString(sha1SumBytes[:])
+	hashSumArray := sha1.Sum(buf) // require go1.2+
+	hashSumHexBytes := make([]byte, 40)
+	hex.Encode(hashSumHexBytes, hashSumArray[:])
 
-	if sha1SumStr == signature {
+	// 现在 len(signature) == 40, 不会 panic
+	if rslt := subtle.ConstantTimeCompare(hashSumHexBytes, []byte(signature)); rslt == 1 {
 		return true
 	}
-
 	return false
 }
