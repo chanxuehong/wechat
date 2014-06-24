391835ed7452ac52743f63780a745a39a2ec6a3e
chanxuehong <chanxuehong@gmail.com>
2014-06-25 07:36:32 +0800

    no message

--- signature.go
+++ signature.go
@@ -9,7 +9,8 @@ import (
 
 // 校验消息是否是从微信服务器发送过来的
 func CheckSignature(signature, timestamp, nonce, token string) bool {
-	if len(signature) != 40 {
+	const hashsumLen = 40 // sha1
+	if len(signature) != hashsumLen {
 		return false
 	}
 
@@ -17,22 +18,24 @@ func CheckSignature(signature, timestamp, nonce, token string) bool {
 	strArr.Sort()
 
 	// buf := []byte(strArr[0] + strArr[1] + strArr[2])
-	// 因为目前 golang 还不支持 copy-on-write, 为了减少内存复制采用了这个的方法
-	str0Len := len(strArr[0])
-	str0str1Len := str0Len + len(strArr[1])
-	str0str1str2Len := str0str1Len + len(strArr[2])
-	buf := make([]byte, str0str1str2Len)
-	copy(buf, strArr[0])
-	copy(buf[str0Len:], strArr[1])
-	copy(buf[str0str1Len:], strArr[2])
+	// 因为目前 golang 还不支持 string 和 []byte 之间的 copy-on-write,
+	// 为了减少内存复制采用了这个的方法
+	hashsumStr0Len := hashsumLen + len(strArr[0])
+	hashsumStr0Str1Len := hashsumStr0Len + len(strArr[1])
+	hashsumStr0Str1Str2Len := hashsumStr0Str1Len + len(strArr[2])
+	buf := make([]byte, hashsumStr0Str1Str2Len)
+	hashsumHexBytes := buf[:hashsumLen]
 
-	hashSumArray := sha1.Sum(buf) // require go1.2+
-	hashSumHexBytes := make([]byte, 40)
-	hex.Encode(hashSumHexBytes, hashSumArray[:])
+	copy(buf[hashsumLen:], strArr[0])
+	copy(buf[hashsumStr0Len:], strArr[1])
+	copy(buf[hashsumStr0Str1Len:], strArr[2])
+	hashsumArray := sha1.Sum(buf[hashsumLen:]) // require go1.2+
+
+	hex.Encode(hashsumHexBytes, hashsumArray[:])
 
 	// 采用 subtle.ConstantTimeCompare 是防止 计时攻击!
-	// 现在 len(signature) == 40, 不会 panic
-	if rslt := subtle.ConstantTimeCompare(hashSumHexBytes, []byte(signature)); rslt == 1 {
+	// 现在 len(signature) == hashsumLen, 不会 panic
+	if rslt := subtle.ConstantTimeCompare(hashsumHexBytes, []byte(signature)); rslt == 1 {
 		return true
 	}
 	return false
