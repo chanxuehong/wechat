bd64aa5c5f5b3ed5f1fa6a44c6c8f097949a6003
chanxuehong <chanxuehong@gmail.com>
2014-06-25 23:39:33 +0800

    no message

--- server/server_signature.go
+++ server/server_signature.go
@@ -7,62 +7,64 @@ import (
 	"sort"
 )
 
-// 校验消息是否是从微信服务器发送过来的
+// 校验消息是否是从微信服务器发送过来的.
 func CheckSignature(signature, timestamp, nonce, token string) bool {
 	const hashsumLen = 40 // sha1
+
 	if len(signature) != hashsumLen {
 		return false
 	}
 
-	strArr := sort.StringSlice{token, timestamp, nonce}
-	strArr.Sort()
+	buf := make([]byte, hashsumLen, hashsumLen+len(timestamp)+len(nonce)+len(token))
 
-	// buf := []byte(strArr[0] + strArr[1] + strArr[2])
-	// 因为目前 golang 还不支持 string 和 []byte 之间的 copy-on-write,
-	// 为了减少内存复制采用了这个的方法
-	hashsumStr0Len := hashsumLen + len(strArr[0])
-	hashsumStr0Str1Len := hashsumStr0Len + len(strArr[1])
-	hashsumStr0Str1Str2Len := hashsumStr0Str1Len + len(strArr[2])
-	buf := make([]byte, hashsumStr0Str1Str2Len)
-	hashsumHexBytes := buf[:hashsumLen]
+	strArray := sort.StringSlice{token, timestamp, nonce}
+	strArray.Sort()
 
-	copy(buf[hashsumLen:], strArr[0])
-	copy(buf[hashsumStr0Len:], strArr[1])
-	copy(buf[hashsumStr0Str1Len:], strArr[2])
+	buf = append(buf, strArray[0]...)
+	buf = append(buf, strArray[1]...)
+	buf = append(buf, strArray[2]...)
+
 	hashsumArray := sha1.Sum(buf[hashsumLen:]) // require go1.2+
 
+	hashsumHexBytes := buf[:hashsumLen]
 	hex.Encode(hashsumHexBytes, hashsumArray[:])
 
 	// 采用 subtle.ConstantTimeCompare 是防止 计时攻击!
-	// 现在 len(signature) == hashsumLen, 不会 panic
 	if rslt := subtle.ConstantTimeCompare(hashsumHexBytes, []byte(signature)); rslt == 1 {
 		return true
 	}
 	return false
 }
 
-// 校验消息是否是从微信服务器发送过来的
-//  NOTE: 确保 len(buf) > 40, 否则会 panic
+// 校验消息是否是从微信服务器发送过来的.
+// 使用 buf 能提高一点性能 和 减少一些对 GC 的压力, buf 的长度最好 >=128
 func CheckSignatureEx(signature, timestamp, nonce, token string, buf []byte) bool {
 	const hashsumLen = 40 // sha1
+
 	if len(signature) != hashsumLen {
 		return false
 	}
 
-	strArr := sort.StringSlice{token, timestamp, nonce}
-	strArr.Sort()
+	bufLen := hashsumLen + len(timestamp) + len(nonce) + len(token)
+	if len(buf) < bufLen {
+		buf = make([]byte, hashsumLen, bufLen)
+	} else {
+		buf = buf[:hashsumLen]
+	}
 
-	buf = buf[:hashsumLen]
-	buf = append(buf, strArr[0]...)
-	buf = append(buf, strArr[1]...)
-	buf = append(buf, strArr[2]...)
+	strArray := sort.StringSlice{token, timestamp, nonce}
+	strArray.Sort()
+
+	buf = append(buf, strArray[0]...)
+	buf = append(buf, strArray[1]...)
+	buf = append(buf, strArray[2]...)
 
 	hashsumArray := sha1.Sum(buf[hashsumLen:]) // require go1.2+
+
 	hashsumHexBytes := buf[:hashsumLen]
 	hex.Encode(hashsumHexBytes, hashsumArray[:])
 
 	// 采用 subtle.ConstantTimeCompare 是防止 计时攻击!
-	// 现在 len(signature) == hashsumLen, 不会 panic
 	if rslt := subtle.ConstantTimeCompare(hashsumHexBytes, []byte(signature)); rslt == 1 {
 		return true
 	}
