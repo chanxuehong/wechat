4b2edfb61eae112994aa45b401f3f3097eb408fb
chanxuehong <chanxuehong@gmail.com>
2014-08-31 18:51:31 +0800

    no message

--- message/request/request.go
+++ message/request/request.go
@@ -24,59 +24,38 @@ type Request struct {
 	CommonHead
 
 	// fuck weixin, MsgId != MsgID
-	MsgId int64 `xml:"MsgId"` // 消息id, 64位整型
-	MsgID int64 `xml:"MsgID"` // 消息id, 64位整型; 高级群发接口的 事件推送群发结果 貌似用的是这个!
+	MsgId int64 `xml:"MsgId"`
+	MsgID int64 `xml:"MsgID"`
 
-	// common message
-	Content      string  `xml:"Content"`      // text, 文本消息内容
-	MediaId      string  `xml:"MediaId"`      // image, voice, video 消息媒体id, 可以调用多媒体文件下载接口拉取数据
-	PicURL       string  `xml:"PicUrl"`       // image, 图片链接
-	Format       string  `xml:"Format"`       // voice, 语音格式, 如amr, speex等
-	Recognition  string  `xml:"Recognition"`  // voice, 语音识别结果, UTF8编码
-	ThumbMediaId string  `xml:"ThumbMediaId"` // video, 视频消息缩略图的媒体id, 可以调用多媒体文件下载接口拉取数据
-	LocationX    float64 `xml:"Location_X"`   // location, 地理位置纬度
-	LocationY    float64 `xml:"Location_Y"`   // location, 地理位置经度
-	Scale        int     `xml:"Scale"`        // location, 地图缩放大小
-	Label        string  `xml:"Label"`        // location, 地理位置信息
-	Title        string  `xml:"Title"`        // link, 消息标题
-	Description  string  `xml:"Description"`  // link, 消息描述
-	URL          string  `xml:"Url"`          // link, 消息链接
+	Content      string  `xml:"Content"`
+	MediaId      string  `xml:"MediaId"`
+	PicURL       string  `xml:"PicUrl"`
+	Format       string  `xml:"Format"`
+	Recognition  string  `xml:"Recognition"`
+	ThumbMediaId string  `xml:"ThumbMediaId"`
+	LocationX    float64 `xml:"Location_X"`
+	LocationY    float64 `xml:"Location_Y"`
+	Scale        int     `xml:"Scale"`
+	Label        string  `xml:"Label"`
+	Title        string  `xml:"Title"`
+	Description  string  `xml:"Description"`
+	URL          string  `xml:"Url"`
 
-	// event message
-	Event     string  `xml:"Event"`     // subscribe, unsubscribe, SCAN, LOCATION, CLICK, VIEW, MASSSENDJOBFINISH
-	EventKey  string  `xml:"EventKey"`  // 不同的 Event 不同的功能
-	Ticket    string  `xml:"Ticket"`    // 二维码的ticket, 可用来换取二维码图片
-	Latitude  float64 `xml:"Latitude"`  // 地理位置纬度
-	Longitude float64 `xml:"Longitude"` // 地理位置经度
-	Precision float64 `xml:"Precision"` // 地理位置精度
-
-	// 群发的结构, 为 "send success" 或 "send fail" 或 "err(num)".
-	// 但 send success 时, 也有可能因用户拒收公众号的消息, 系统错误等原因造成少量用户接收失败.
-	// err(num) 是审核失败的具体原因, 可能的情况如下:
-	// err(10001), //涉嫌广告
-	// err(20001), //涉嫌政治
-	// err(20004), //涉嫌社会
-	// err(20002), //涉嫌色情
-	// err(20006), //涉嫌违法犯罪
-	// err(20008), //涉嫌欺诈
-	// err(20013), //涉嫌版权
-	// err(22000), //涉嫌互推(互相宣传)
-	// err(21000), //涉嫌其他
-	Status string `xml:"Status"`
-
-	TotalCount int `xml:"TotalCount"` // group_id 下粉丝数, 或者 openid_list 中的粉丝数
-
-	// 过滤(过滤是指特定地区, 性别的过滤, 用户设置拒收的过滤; 用户接收已超4条的过滤）后,
-	// 准备发送的粉丝数, 原则上, FilterCount = SentCount + ErrorCount
-	FilterCount int `xml:"FilterCount"`
-	SentCount   int `xml:"SentCount"`  // 发送成功的粉丝数
-	ErrorCount  int `xml:"ErrorCount"` // 发送失败的粉丝数
-
-	// 微信小店, 订单付款通知
-	OrderId     string `xml:"OrderId"`
-	OrderStatus int    `xml:"OrderStatus"`
-	ProductId   string `xml:"ProductId"`
-	SkuInfo     string `xml:"SkuInfo"`
+	Event       string  `xml:"Event"`
+	EventKey    string  `xml:"EventKey"`
+	Ticket      string  `xml:"Ticket"`
+	Latitude    float64 `xml:"Latitude"`
+	Longitude   float64 `xml:"Longitude"`
+	Precision   float64 `xml:"Precision"`
+	Status      string  `xml:"Status"`
+	TotalCount  int     `xml:"TotalCount"`
+	FilterCount int     `xml:"FilterCount"`
+	SentCount   int     `xml:"SentCount"`
+	ErrorCount  int     `xml:"ErrorCount"`
+	OrderId     string  `xml:"OrderId"`
+	OrderStatus int     `xml:"OrderStatus"`
+	ProductId   string  `xml:"ProductId"`
+	SkuInfo     string  `xml:"SkuInfo"`
 }
 
 var zeroRequest Request
---
+++ message/template/msg_test.go
@@ -0,0 +1,143 @@
+// @description wechat 是腾讯微信公众平台 api 的 golang 语言封装
+// @link        https://github.com/chanxuehong/wechat for the canonical source repository
+// @license     https://github.com/chanxuehong/wechat/blob/master/LICENSE
+// @authors     chanxuehong(chanxuehong@gmail.com)
+
+package template
+
+import (
+	"bytes"
+	"encoding/json"
+	"github.com/chanxuehong/util"
+	"testing"
+)
+
+func TestMarshalAndNewFunc(t *testing.T) {
+	var expectBytes []byte
+
+	// 纯文本模板消息==============================================================
+
+	expectBytes = []byte(`{
+	    "touser":"OPENID",
+	    "template_id":"aygtGTLdrjHJP7Bu4EdkptNfYaeFKi98ygn2kitCJ6fAfdmN88naVvX6V5uIV5x0",
+	    "data":{
+	        "Goods":"苹果",
+	        "Unit_price":"RMB 20.13",
+	        "Quantity":"5",
+	        "Total":"RMB 100.65",
+	        "Source":{
+	            "Shop":"Jas屌丝商店",
+	            "Recommend":"5颗星"
+	        }
+	    }
+	}`)
+
+	var data struct {
+		Goods      string
+		Unit_price string
+		Quantity   string
+		Total      string
+		Source     struct {
+			Shop      string
+			Recommend string
+		}
+	}
+	data.Goods = "苹果"
+	data.Unit_price = "RMB 20.13"
+	data.Quantity = "5"
+	data.Total = "RMB 100.65"
+	data.Source.Shop = "Jas屌丝商店"
+	data.Source.Recommend = "5颗星"
+
+	msg := NewMsg("OPENID", "aygtGTLdrjHJP7Bu4EdkptNfYaeFKi98ygn2kitCJ6fAfdmN88naVvX6V5uIV5x0", &data)
+
+	b, err := json.Marshal(msg)
+	if err != nil {
+		t.Errorf("json.Marshal(%#q):\nError: %s\n", msg, err)
+	} else {
+		want := util.TrimSpace(expectBytes)
+		if !bytes.Equal(b, want) {
+			t.Errorf("json.Marshal(%#q):\nhave %#s\nwant %#s\n", msg, b, want)
+		}
+	}
+
+	// 链接的模板消息==============================================================
+
+	expectBytes = []byte(`{
+	    "touser":"OPENID",
+	    "template_id":"ngqIpbwh8bUfcSsECmogfXcV14J0tQlEpBO27izEYtY",
+	    "data":{
+	        "User":{
+	            "value":"黄先生",
+	            "color":"#173177"
+	        },
+	        "Date":{
+	            "value":"06月07日 19时24分",
+	            "color":"#173177"
+	        },
+	        "CardNumber":{
+	            "value":"0426",
+	            "color":"#173177"
+	        },
+	        "Type":{
+	            "value":"消费",
+	            "color":"#173177"
+	        },
+	        "Money":{
+	            "value":"人民币260.00元",
+	            "color":"#173177"
+	        },
+	        "DeadTime":{
+	            "value":"06月07日19时24分",
+	            "color":"#173177"
+	        },
+	        "Left":{
+	            "value":"6504.09",
+	            "color":"#173177"
+	        }
+	    },
+		"url":"http://weixin.qq.com/download",
+	    "topcolor":"#FF0000"
+	}`)
+
+	type DateValue struct {
+		Value string `json:"value"`
+		Color string `json:"color"`
+	}
+	var data1 struct {
+		User       DateValue
+		Date       DateValue
+		CardNumber DateValue
+		Type       DateValue
+		Money      DateValue
+		DeadTime   DateValue
+		Left       DateValue
+	}
+	data1.User.Value = "黄先生"
+	data1.User.Color = "#173177"
+	data1.Date.Value = "06月07日 19时24分"
+	data1.Date.Color = "#173177"
+	data1.CardNumber.Value = "0426"
+	data1.CardNumber.Color = "#173177"
+	data1.Type.Value = "消费"
+	data1.Type.Color = "#173177"
+	data1.Money.Value = "人民币260.00元"
+	data1.Money.Color = "#173177"
+	data1.DeadTime.Value = "06月07日19时24分"
+	data1.DeadTime.Color = "#173177"
+	data1.Left.Value = "6504.09"
+	data1.Left.Color = "#173177"
+
+	msg1 := NewMsgWithLink("OPENID", "ngqIpbwh8bUfcSsECmogfXcV14J0tQlEpBO27izEYtY",
+		&data1, "http://weixin.qq.com/download", "#FF0000")
+
+	b, err = json.Marshal(msg1)
+	if err != nil {
+		t.Errorf("json.Marshal(%#q):\nError: %s\n", msg1, err)
+	} else {
+		want := util.TrimSpace(expectBytes)
+		if !bytes.Equal(b, want) {
+			t.Errorf("json.Marshal(%#q):\nhave %#s\nwant %#s\n", msg1, b, want)
+		}
+	}
+}
