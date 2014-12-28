a8e457f9aa616e5f2dc61fbd7015ba9266cd482a
magicshui <shuiyuzhe@gmail.com>
2014-12-28 19:21:59 +0800

    添加针对所有人的群发属性
    参考：http://mp.weixin.qq.com/wiki/15/5380a4e6f02f2ffdc7981a8ed7a40753.html
    #.E6.9F.A5.E8.AF.A2.E7.BE.A4.E5.8F.91.E6.B6.88.E6.81.AF.E5.8F.91.E9.80.8
    1.E7.8A.B6.E6.80.81.E3.80.90.E8.AE.A2.E9.98.85.E5.8F.B7.E4.B8.8E.E6.9C.8
    D.E5.8A.A1.E5.8F.B7.E8.AE.A4.E8.AF.81.E5.90.8E.E5.9D.87.E5.8F.AF.E7.94.A
    8.E3.80.91

--- mp/message/active/massbygroup/msg.go
+++ mp/message/active/massbygroup/msg.go
@@ -7,6 +7,7 @@ package massbygroup
 
 type CommonHead struct {
 	Filter struct {
+		IsToAll bool  `json:"is_to_all"`
 		GroupId int64 `json:"group_id,string"`
 	} `json:"filter"`
 	MsgType string `json:"msgtype"`
@@ -22,8 +23,9 @@ type Text struct {
 }
 
 // 新建文本消息
-func NewText(groupId int64, content string) *Text {
+func NewText(isToAll bool, groupId int64, content string) *Text {
 	var msg Text
+	msg.Filter.IsToAll = isToAll
 	msg.Filter.GroupId = groupId
 	msg.MsgType = MSG_TYPE_TEXT
 	msg.Text.Content = content
@@ -42,8 +44,9 @@ type Image struct {
 
 // 新建图片消息
 //  mediaId 通过上传多媒体文件得到
-func NewImage(groupId int64, mediaId string) *Image {
+func NewImage(isToAll bool, groupId int64, mediaId string) *Image {
 	var msg Image
+	msg.Filter.IsToAll = isToAll
 	msg.Filter.GroupId = groupId
 	msg.MsgType = MSG_TYPE_IMAGE
 	msg.Image.MediaId = mediaId
@@ -62,8 +65,9 @@ type Voice struct {
 
 // 新建语音消息
 //  mediaId 通过上传多媒体文件得到
-func NewVoice(groupId int64, mediaId string) *Voice {
+func NewVoice(isToAll bool, groupId int64, mediaId string) *Voice {
 	var msg Voice
+	msg.Filter.IsToAll = isToAll
 	msg.Filter.GroupId = groupId
 	msg.MsgType = MSG_TYPE_VOICE
 	msg.Voice.MediaId = mediaId
@@ -82,8 +86,9 @@ type Video struct {
 
 // 新建视频消息
 //  NOTE: mediaId 应该通过 Client.MediaCreateVideo 得到
-func NewVideo(groupId int64, mediaId string) *Video {
+func NewVideo(isToAll bool, groupId int64, mediaId string) *Video {
 	var msg Video
+	msg.Filter.IsToAll = isToAll
 	msg.Filter.GroupId = groupId
 	msg.MsgType = MSG_TYPE_VIDEO
 	msg.Video.MediaId = mediaId
@@ -102,8 +107,9 @@ type News struct {
 
 // 新建图文消息
 //  NOTE: mediaId 应该通过 Client.MediaCreateNews 得到
-func NewNews(groupId int64, mediaId string) *News {
+func NewNews(isToAll bool, groupId int64, mediaId string) *News {
 	var msg News
+	msg.Filter.IsToAll = isToAll
 	msg.Filter.GroupId = groupId
 	msg.MsgType = MSG_TYPE_NEWS
 	msg.News.MediaId = mediaId
