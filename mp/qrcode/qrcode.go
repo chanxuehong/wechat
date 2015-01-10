1b8039b2b013b7a18718a1eaf82addc62397eb94
chan xuehong <chanxuehong@gmail.com>
2015-01-10 16:06:32 +0800

    二維碼新增了一個 url 字段，原來的 URL 方法改成 PicURL 方法

--- mp/qrcode/qrcode.go
+++ mp/qrcode/qrcode.go
@@ -13,10 +13,11 @@ import (
 type PermanentQRCode struct {
 	SceneId uint32 `json:"scene_id"` // 场景值 id, 目前参数只支持1--100000
 	Ticket  string `json:"ticket"`   // 二维码ticket, 凭借此ticket可以在有效时间内换取二维码.
+	URL     string `json:"url"`      // 二维码图片解析后的地址，开发者可根据该地址自行生成需要的二维码图片
 }
 
-// 二维码的 URL, 可以 GET 此 URL 下载二维码
-func (qrcode *PermanentQRCode) URL() string {
+// 二维码圖片的 URL, 可以 GET 此 URL 下载二维码
+func (qrcode *PermanentQRCode) PicURL() string {
 	return "https://mp.weixin.qq.com/cgi-bin/showqrcode?ticket=" +
 		url.QueryEscape(qrcode.Ticket)
 }
@@ -25,11 +26,12 @@ func (qrcode *PermanentQRCode) URL() string {
 type TemporaryQRCode struct {
 	SceneId   uint32 `json:"scene_id"`       // 场景值 id, 32位非0整型
 	Ticket    string `json:"ticket"`         // 二维码ticket, 凭借此ticket可以在有效时间内换取二维码.
+	URL       string `json:"url"`            // 二维码图片解析后的地址，开发者可根据该地址自行生成需要的二维码图片
 	ExpiresIn int    `json:"expire_seconds"` // 有效期, 单位为"秒"
 }
 
-// 二维码的 URL, 可以 GET 此 URL 下载二维码
-func (qrcode *TemporaryQRCode) URL() string {
+// 二维码圖片的 URL, 可以 GET 此 URL 下载二维码
+func (qrcode *TemporaryQRCode) PicURL() string {
 	return "https://mp.weixin.qq.com/cgi-bin/showqrcode?ticket=" +
 		url.QueryEscape(qrcode.Ticket)
 }
