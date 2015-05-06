095399042677bd18d9fe8f22bbb7c5d9b8bf7e4a
chan xuehong <chanxuehong@gmail.com>
2015-05-06 18:49:54 +0800

    Revert "no message"
    This reverts commit d9eb1da6093dc07160b52bd616bf5b302e50c9e1.

--- mch/pay/log.go
+++ mch/pay/log.go
@@ -9,7 +9,15 @@ import (
 	"log"
 )
 
-var LogInfoln = log.Println // 簡單起見, 硬編碼了
+var LogInfoln = log.Println
+
+// 沒有加锁, 请确保在初始化阶段调用!
+func SetLogInfoln(fn func(v ...interface{})) {
+	if fn == nil {
+		return
+	}
+	LogInfoln = fn
+}
 
 func init() {
 	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
