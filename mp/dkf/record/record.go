d1a9c5a571db386ac2d3781efb203d8e52e1cfbb
chanxuehong <chanxuehong@gmail.com>
2015-07-10 13:55:05 +0800

    多客服接口更新

--- mp/dkf/record/record.go
+++ mp/dkf/record/record.go
@@ -24,11 +24,11 @@ const (
 )
 
 type GetRecordRequest struct {
-	StartTime int64 `json:"starttime"` // 查询开始时间, UNIX时间戳
-	EndTime   int64 `json:"endtime"`   // 查询结束时间, UNIX时间戳, 每次查询不能跨日查询
-	PageIndex int   `json:"pageindex"` // 查询第几页, 从1开始
-	PageSize  int   `json:"pagesize"`  // 每页大小, 每页最多拉取50条
-	//OpenId    string `json:"openid,omitempty"` // 普通用户的标识, 对当前公众号唯一
+	StartTime int64  `json:"starttime"`        // 查询开始时间, UNIX时间戳
+	EndTime   int64  `json:"endtime"`          // 查询结束时间, UNIX时间戳, 每次查询不能跨日查询
+	PageIndex int    `json:"pageindex"`        // 查询第几页, 从1开始
+	PageSize  int    `json:"pagesize"`         // 每页大小, 每页最多拉取50条
+	OpenId    string `json:"openid,omitempty"` // 普通用户的标识, 对当前公众号唯一
 }
 
 // 获取客服聊天记录
