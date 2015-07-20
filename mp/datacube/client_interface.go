4a89a13f22355278bb35bb9a74a04be72c43e744
chanxuehong <chanxuehong@gmail.com>
2015-07-20 15:32:29 +0800

    datacube 更新

--- mp/datacube/client_interface.go
+++ mp/datacube/client_interface.go
@@ -16,8 +16,8 @@ type InterfaceSummaryData struct {
 	RefDate       string `json:"ref_date"`        // 数据的日期, YYYY-MM-DD 格式
 	CallbackCount int    `json:"callback_count"`  // 通过服务器配置地址获得消息后, 被动回复用户消息的次数
 	FailCount     int    `json:"fail_count"`      // 上述动作的失败次数
-	TotalTimeCost int    `json:"total_time_cost"` // 总耗时, 除以callback_count即为平均耗时
-	MaxTimeCost   int    `json:"max_time_cost"`   // 最大耗时
+	TotalTimeCost int64  `json:"total_time_cost"` // 总耗时, 除以callback_count即为平均耗时
+	MaxTimeCost   int64  `json:"max_time_cost"`   // 最大耗时
 }
 
 // 获取接口分析数据.
@@ -46,8 +46,8 @@ func (clt Client) GetInterfaceSummary(req *Request) (list []InterfaceSummaryData
 }
 
 type InterfaceSummaryHourData struct {
+	RefHour int `json:"ref_hour"` // 数据的小时, 包括从000到2300, 分别代表的是[000,100)到[2300,2400), 即每日的第1小时和最后1小时
 	InterfaceSummaryData
-	RefHour *int `json:"ref_hour,omitempty"` // 数据的小时, 包括从000到2300, 分别代表的是[000,100)到[2300,2400), 即每日的第1小时和最后1小时
 }
 
 // 获取接口分析分时数据.
