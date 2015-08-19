d9567794dcde64964774104c4e8ee6f5079127b4
chan xuehong <chanxuehong@gmail.com>
2015-08-19 12:13:59 +0800

    微信支付下载对账单接口更新

--- mch/pay/common.go
+++ mch/pay/common.go
@@ -8,9 +8,11 @@ package pay
 import (
 	"bytes"
 	"encoding/xml"
+	"errors"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
+	"os"
 
 	"github.com/chanxuehong/util"
 	"github.com/chanxuehong/wechat/mch"
@@ -42,18 +44,61 @@ func RefundQuery(pxy *mch.Proxy, req map[string]string) (resp map[string]string,
 	return pxy.PostXML("https://api.mch.weixin.qq.com/pay/refundquery", req)
 }
 
-// 下载对账单.
-func DownloadBill(req map[string]string, httpClient *http.Client) (data []byte, err error) {
-	if httpClient == nil {
-		httpClient = http.DefaultClient
-	}
-
-	bodyBuf := bytes.NewBuffer(make([]byte, 0, 1024))
-	if err = util.FormatMapToXML(bodyBuf, req); err != nil {
+// 下载对账单到到文件.
+func DownloadBill(filepath string, req map[string]string, httpClient *http.Client) (written int64, err error) {
+	if req == nil {
+		err = errors.New("nil request req")
 		return
 	}
 
-	httpResp, err := httpClient.Post("https://api.mch.weixin.qq.com/pay/downloadbill", "text/xml; charset=utf-8", bodyBuf)
+	file, err := os.Create(filepath)
+	if err != nil {
+		return
+	}
+	defer func() {
+		file.Close()
+		if err != nil {
+			os.Remove(filepath)
+		}
+	}()
+
+	return downloadBillToWriter(file, req, httpClient)
+}
+
+// 下载对账单到 io.Writer.
+func DownloadBillToWriter(writer io.Writer, req map[string]string, httpClient *http.Client) (written int64, err error) {
+	if writer == nil {
+		err = errors.New("nil writer")
+		return
+	}
+	if req == nil {
+		err = errors.New("nil request req")
+		return
+	}
+	return downloadBillToWriter(writer, req, httpClient)
+}
+
+var (
+	// <xml><return_code><![CDATA[FAIL]]></return_code>
+	// <return_msg><![CDATA[require POST method]]></return_msg>
+	// </xml>
+	downloadBillErrorRootNodeStartElement       = []byte("<xml>")
+	downloadBillErrorReturnCodeNodeStartElement = []byte("<return_code>")
+)
+
+// 下载对账单到 io.Writer.
+func downloadBillToWriter(writer io.Writer, req map[string]string, httpClient *http.Client) (written int64, err error) {
+	if httpClient == nil {
+		httpClient = http.DefaultClient
+	}
+	buf := make([]byte, 32*1024) // 与 io.copyBuffer 里的默认大小一致
+
+	reqBuf := bytes.NewBuffer(buf[:0])
+	if err = util.FormatMapToXML(reqBuf, req); err != nil {
+		return
+	}
+
+	httpResp, err := httpClient.Post("https://api.mch.weixin.qq.com/pay/downloadbill", "text/xml; charset=utf-8", reqBuf)
 	if err != nil {
 		return
 	}
@@ -64,18 +109,36 @@ func DownloadBill(req map[string]string, httpClient *http.Client) (data []byte, 
 		return
 	}
 
-	respBody, err := ioutil.ReadAll(httpResp.Body)
-	if err != nil {
+	n, err := io.ReadFull(httpResp.Body, buf)
+	switch {
+	case err == nil:
+		// n == len(buf), 可以认为返回的是对账单而不是xml格式的错误信息
+		written, err = bytes.NewReader(buf).WriteTo(writer)
+		if err != nil {
+			return
+		}
+		var n2 int64
+		n2, err = io.CopyBuffer(writer, httpResp.Body, buf)
+		written += n2
+		return
+	case err == io.ErrUnexpectedEOF:
+		readBytes := buf[:n]
+		if index := bytes.Index(readBytes, downloadBillErrorRootNodeStartElement); index != -1 {
+			if bytes.Contains(readBytes[index+len(downloadBillErrorRootNodeStartElement):], downloadBillErrorReturnCodeNodeStartElement) {
+				// 可以认为是错误信息了, 尝试解析xml
+				var result mch.Error
+				if err = xml.Unmarshal(readBytes, &result); err == nil {
+					err = &result
+					return
+				}
+				// err != nil 执行默认的动作, 写入 writer
+			}
+		}
+		return bytes.NewReader(readBytes).WriteTo(writer)
+	case err == io.EOF: // 返回空的body
+		err = nil
+		return
+	default: // 其他的错误
 		return
 	}
-
-	var result mch.Error
-	if err = xml.Unmarshal(respBody, &result); err == nil {
-		err = &result
-		return
-	}
-
-	data = respBody
-	err = nil
-	return
 }
