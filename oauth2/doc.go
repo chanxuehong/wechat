b0b0f54a2311f773add38b74946baa9f5520ee7b
chan xuehong <chanxuehong@gmail.com>
2015-10-12 12:58:13 +0800

    no message

---
+++ oauth2/doc.go
@@ -0,0 +1,7 @@
+// @description wechat 是腾讯微信公众平台 api 的 golang 语言封装
+// @link        https://github.com/chanxuehong/wechat for the canonical source repository
+// @license     https://github.com/chanxuehong/wechat/blob/master/LICENSE
+// @authors     chanxuehong(chanxuehong@gmail.com)
+
+// 微信相关的 oauth2 基础库
+package oauth2
