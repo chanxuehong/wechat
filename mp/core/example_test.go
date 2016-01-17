a851ee4b3c6d3bc0419635d32e8652777b6d34ed
chanxuehong <chanxuehong@gmail.com>
2016-01-17 12:57:45 +0800

    init

--- mp/core/example_test.go
+++ mp/core/example_test.go
@@ -8,24 +8,35 @@ import (
 
 func ExampleServer_ServeHTTP() {
 	// 在回调 URL 的 Handler 里处理消息(事件)
-	http.HandleFunc("/wecaht_callback", func(w http.ResponseWriter, r *http.Request) {
-		// 创建默认的 core.Handler, 也可以用自己实现的 core.Handler
-		mux := core.NewServeMux()
-		// 注册中间件
-		mux.UseFunc(func(ctx *core.Context) {
+	http.HandleFunc("/wechat_callback", func(w http.ResponseWriter, r *http.Request) {
+		mux := core.NewServeMux() // 创建默认的 core.Handler, 也可以用自己实现的 core.Handler
+
+		mux.UseFunc(func(ctx *core.Context) { // 注册中间件, 处理所有的消息(事件)
 			// TODO: 中间件处理逻辑
 		})
-		// 设置默认消息处理 Handler
-		mux.DefaultMsgHandleFunc(func(ctx *core.Context) {
+		mux.UseFuncForMsg(func(ctx *core.Context) { // 注册中间件, 处理所有的消息
+			// TODO: 中间件处理逻辑
+		})
+		mux.UseFuncForEvent(func(ctx *core.Context) { // 注册中间件, 处理所有的事件
+			// TODO: 中间件处理逻辑
+		})
+
+		mux.DefaultMsgHandleFunc(func(ctx *core.Context) { // 设置默认消息处理 Handler
 			// TODO: 消息处理逻辑
 		})
-		// 设置默认事件处理 Handler
-		mux.DefaultEventHandleFunc(func(ctx *core.Context) {
+		mux.DefaultEventHandleFunc(func(ctx *core.Context) { // 设置默认事件处理 Handler
+			// TODO: 事件处理逻辑
+		})
+
+		mux.MsgHandleFunc("{MsgType}", func(ctx *core.Context) { // 设置具体类型的消息处理 Handler
+			// TODO: 消息处理逻辑
+		})
+		mux.EventHandleFunc("{EventType}", func(ctx *core.Context) { // 设置具体类型的事件处理 Handler
 			// TODO: 事件处理逻辑
 		})
 
 		// 创建 Server, 设置正确的参数
-		srv := core.NewServer("", "", "{token}", "", mux, nil)
+		srv := core.NewServer("{oriId}", "{appId}", "{token}", "{base64AESKey}", mux, nil)
 
 		// 处理回调请求
 		srv.ServeHTTP(w, r, nil)
