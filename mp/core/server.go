cfdc64ab285ef77d5ed5bbd52433911af6766cdd
chanxuehong <chanxuehong@gmail.com>
2016-04-14 11:12:07 +0800

    callback 优化掉一个xml解析

--- mp/core/server.go
+++ mp/core/server.go
@@ -8,11 +8,13 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"net/url"
 	"strconv"
 	"sync"
 	"sync/atomic"
+	"unicode"
 	"unsafe"
 
 	"github.com/chanxuehong/util/security"
@@ -252,12 +254,18 @@ func (srv *Server) ServeHTTP(w http.ResponseWriter, r *http.Request, queryParams
 				}
 			}
 
-			var requestHttpBody struct {
-				XMLName            struct{} `xml:"xml"`
-				ToUserName         string   `xml:"ToUserName"`
-				Base64EncryptedMsg []byte   `xml:"Encrypt"`
+			requestBodyBuf := textBufferPool.Get().(*bytes.Buffer)
+			requestBodyBuf.Reset()
+			defer textBufferPool.Put(requestBodyBuf)
+
+			if _, err = requestBodyBuf.ReadFrom(r.Body); err != nil {
+				errorHandler.ServeError(w, r, err)
+				return
 			}
-			if err = xml.NewDecoder(r.Body).Decode(&requestHttpBody); err != nil {
+			requestBodyBytes := requestBodyBuf.Bytes()
+
+			var requestHttpBody cipherRequestHttpBody
+			if err = xmlUnmarshal(requestBodyBytes, &requestHttpBody); err != nil {
 				errorHandler.ServeError(w, r, err)
 				return
 			}
@@ -505,4 +513,102 @@ func (srv *Server) ServeHTTP(w http.ResponseWriter, r *http.Request, queryParams
 
 		io.WriteString(w, echostr)
 	}
+}
+
+// =====================================================================================================================
+
+type cipherRequestHttpBody struct {
+	XMLName            struct{} `xml:"xml"`
+	ToUserName         string   `xml:"ToUserName"`
+	Base64EncryptedMsg []byte   `xml:"Encrypt"`
+}
+
+var (
+	msgStartElementLiteral = []byte("<xml>")
+	msgEndElementLiteral   = []byte("</xml>")
+
+	msgToUserNameStartElementLiteral = []byte("<ToUserName>")
+	msgToUserNameEndElementLiteral   = []byte("</ToUserName>")
+
+	msgEncryptStartElementLiteral = []byte("<Encrypt>")
+	msgEncryptEndElementLiteral   = []byte("</Encrypt>")
+
+	cdataStartLiteral = []byte("<![CDATA[")
+	cdataEndLiteral   = []byte("]]>")
+)
+
+//<xml>
+//    <ToUserName><![CDATA[gh_b1eb3f8bd6c6]]></ToUserName>
+//    <Encrypt><![CDATA[DlCGq+lWQuyjNNK+vDaO0zUltpdUW3u4V00WCzsdNzmZGEhrU7TPxG52viOKCWYPwTMbCzgbCtakZHyNxr5hjoZJ7ORAUYoIAGQy/LDWtAnYgDO+ppKLp0rDq+67Dv3yt+vatMQTh99NII6x9SEGpY3O2h8RpG99+NYevQiOLVKqiQYzan21sX/jE4Y3wZaeudsb4QVjqzRAPaCJ5nS3T31uIR9fjSRgHTDRDOzjQ1cHchge+t6faUhniN5VQVTE+wIYtmnejc55BmHYPfBnTkYah9+cTYnI3diUPJRRiyVocJyHlb+XOZN22dsx9yzKHBAyagaoDIV8Yyb/PahcUbsqGv5wziOgLJQIa6z93/VY7d2Kq2C2oBS+Qb+FI9jLhgc3RvCi+Yno2X3cWoqbsRwoovYdyg6jme/H7nMZn77PSxOGRt/dYiWx2NuBAF7fNFigmbRiive3DyOumNCMvA==]]></Encrypt>
+//</xml>
+func xmlUnmarshal(data []byte, p *cipherRequestHttpBody) error {
+	data = bytes.TrimSpace(data)
+	if !bytes.HasPrefix(data, msgStartElementLiteral) || !bytes.HasSuffix(data, msgEndElementLiteral) {
+		log.Printf("[WARNING] xmlUnmarshal failed, data:\n%s\n", data)
+		return xml.Unmarshal(data, p)
+	}
+	data2 := data[len(msgStartElementLiteral) : len(data)-len(msgEndElementLiteral)]
+
+	// ToUserName
+	ToUserNameElementBytes := data2
+	i := bytes.Index(ToUserNameElementBytes, msgToUserNameStartElementLiteral)
+	if i == -1 {
+		log.Printf("[WARNING] xmlUnmarshal failed, data:\n%s\n", data)
+		return xml.Unmarshal(data, p)
+	}
+	ToUserNameElementBytes = ToUserNameElementBytes[i+len(msgToUserNameStartElementLiteral):]
+	ToUserNameElementBytes = bytes.TrimLeftFunc(ToUserNameElementBytes, unicode.IsSpace)
+	if !bytes.HasPrefix(ToUserNameElementBytes, cdataStartLiteral) {
+		log.Printf("[WARNING] xmlUnmarshal failed, data:\n%s\n", data)
+		return xml.Unmarshal(data, p)
+	}
+	ToUserNameElementBytes = ToUserNameElementBytes[len(cdataStartLiteral):]
+	i = bytes.Index(ToUserNameElementBytes, cdataEndLiteral)
+	if i == -1 {
+		log.Printf("[WARNING] xmlUnmarshal failed, data:\n%s\n", data)
+		return xml.Unmarshal(data, p)
+	}
+	ToUserName := ToUserNameElementBytes[:i]
+	ToUserNameElementBytes = ToUserNameElementBytes[i+len(cdataEndLiteral):]
+	ToUserNameElementBytes = bytes.TrimLeftFunc(ToUserNameElementBytes, unicode.IsSpace)
+	if !bytes.HasPrefix(ToUserNameElementBytes, msgToUserNameEndElementLiteral) {
+		log.Printf("[WARNING] xmlUnmarshal failed, data:\n%s\n", data)
+		return xml.Unmarshal(data, p)
+	}
+	ToUserNameElementBytes = ToUserNameElementBytes[len(msgToUserNameEndElementLiteral):]
+
+	// Encrypt
+	EncryptElementBytes := ToUserNameElementBytes
+	i = bytes.Index(EncryptElementBytes, msgEncryptStartElementLiteral)
+	if i == -1 {
+		EncryptElementBytes = data2
+		i = bytes.Index(EncryptElementBytes, msgEncryptStartElementLiteral)
+		if i == -1 {
+			log.Printf("[WARNING] xmlUnmarshal failed, data:\n%s\n", data)
+			return xml.Unmarshal(data, p)
+		}
+	}
+	EncryptElementBytes = EncryptElementBytes[i+len(msgEncryptStartElementLiteral):]
+	EncryptElementBytes = bytes.TrimLeftFunc(EncryptElementBytes, unicode.IsSpace)
+	if !bytes.HasPrefix(EncryptElementBytes, cdataStartLiteral) {
+		log.Printf("[WARNING] xmlUnmarshal failed, data:\n%s\n", data)
+		return xml.Unmarshal(data, p)
+	}
+	EncryptElementBytes = EncryptElementBytes[len(cdataStartLiteral):]
+	i = bytes.Index(EncryptElementBytes, cdataEndLiteral)
+	if i == -1 {
+		log.Printf("[WARNING] xmlUnmarshal failed, data:\n%s\n", data)
+		return xml.Unmarshal(data, p)
+	}
+	Encrypt := EncryptElementBytes[:i]
+	EncryptElementBytes = EncryptElementBytes[i+len(cdataEndLiteral):]
+	EncryptElementBytes = bytes.TrimLeftFunc(EncryptElementBytes, unicode.IsSpace)
+	if !bytes.HasPrefix(EncryptElementBytes, msgEncryptEndElementLiteral) {
+		log.Printf("[WARNING] xmlUnmarshal failed, data:\n%s\n", data)
+		return xml.Unmarshal(data, p)
+	}
+
+	p.ToUserName = string(ToUserName)
+	p.Base64EncryptedMsg = Encrypt
+	return nil
 }
