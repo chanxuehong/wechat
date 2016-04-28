fbbf2bb276a8c916526f9da0817f4d8293f35ade
chan <chanxuehong@gmail.com>
2016-04-28 17:38:13 +0800

    微信支付增加 jssdk 签名

--- mch/core/sign.go
+++ mch/core/sign.go
@@ -58,6 +58,71 @@ func Sign(params map[string]string, apiKey string, fn func() hash.Hash) string {
 	return string(bytes.ToUpper(signature))
 }
 
+// jssdk 支付签名, signType 只支持 "MD5", "SHA1", 传入其他的值会 panic.
+func JsapiSign(appId, timeStamp, nonceStr, packageStr, signType string, apiKey string) string {
+	var h hash.Hash
+	switch signType {
+	case "MD5":
+		h = md5.New()
+	case "SHA1":
+		h = sha1.New()
+	default:
+		panic("unsupported signType")
+	}
+
+	var buf0 [256]byte
+	buf1 := buf0[:]
+
+	// appId
+	// nonceStr
+	// package
+	// signType
+	// timeStamp
+	if appId != "" {
+		buf1 = buf1[:0]
+		buf1 = append(buf1, "appId="...)
+		buf1 = append(buf1, appId...)
+		h.Write(buf1)
+	}
+	if nonceStr != "" {
+		buf1 = buf1[:0]
+		buf1 = append(buf1, "&nonceStr="...)
+		buf1 = append(buf1, nonceStr...)
+		h.Write(buf1)
+	}
+	if packageStr != "" {
+		buf1 = buf1[:0]
+		buf1 = append(buf1, "&package="...)
+		buf1 = append(buf1, packageStr...)
+		h.Write(buf1)
+	}
+	if signType != "" {
+		buf1 = buf1[:0]
+		buf1 = append(buf1, "&signType="...)
+		buf1 = append(buf1, signType...)
+		h.Write(buf1)
+	}
+	if timeStamp != "" {
+		buf1 = buf1[:0]
+		buf1 = append(buf1, "&timeStamp="...)
+		buf1 = append(buf1, timeStamp...)
+		h.Write(buf1)
+	}
+	buf1 = buf1[:0]
+	buf1 = append(buf1, "&key="...)
+	buf1 = append(buf1, apiKey...)
+	h.Write(buf1)
+
+	var signature []byte
+	if size := hex.EncodedLen(h.Size()); size > len(buf0) {
+		signature = make([]byte, size)
+	} else {
+		signature = buf0[:size]
+	}
+	hex.Encode(signature, h.Sum(nil))
+	return string(bytes.ToUpper(signature))
+}
+
 // EditAddressSign 收货地址共享接口签名
 func EditAddressSign(appId, url, timestamp, nonceStr, accessToken string) string {
 	var buf0 [256]byte
