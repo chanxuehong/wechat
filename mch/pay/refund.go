683899ab92cc59c3f3304d238629812b12b618b4
chan <chanxuehong@gmail.com>
2016-06-06 21:12:25 +0800

    fuck weixin

--- mch/pay/refund.go
+++ mch/pay/refund.go
@@ -32,21 +32,21 @@ type RefundResponse struct {
 	AppId string // 微信分配的公众账号ID
 	MchId string // 微信支付分配的商户号
 
-	TransactionId       string // 微信订单号
-	OutTradeNo          string // 商户系统内部的订单号
-	OutRefundNo         string // 商户退款单号
-	RefundId            string // 微信退款单号
-	RefundFee           int64  // 退款总金额,单位为分,可以做部分退款
-	SettlementRefundFee int64  // 退款金额=申请退款金额-非充值代金券退款金额，退款金额<=申请退款金额
-	TotalFee            int64  // 订单总金额，单位为分，只能为整数，详见支付金额
-	SettlementTotalFee  int64  // 应结订单金额=订单金额-非充值代金券金额，应结订单金额<=订单金额。
-	CashFee             int64  // 现金支付金额，单位为分，只能为整数，详见支付金额
+	TransactionId string // 微信订单号
+	OutTradeNo    string // 商户系统内部的订单号
+	OutRefundNo   string // 商户退款单号
+	RefundId      string // 微信退款单号
+	RefundFee     int64  // 退款总金额,单位为分,可以做部分退款
+	TotalFee      int64  // 订单总金额，单位为分，只能为整数，详见支付金额
+	CashFee       int64  // 现金支付金额，单位为分，只能为整数，详见支付金额
 
 	// 下面字段都是可选返回的(详细见微信支付文档), 为空值表示没有返回, 程序逻辑里需要判断
-	DeviceInfo    string // 微信支付分配的终端设备号，与下单一致
-	RefundChannel string // 退款渠道
-	FeeType       string // 订单金额货币类型，符合ISO 4217标准的三位字母代码，默认人民币：CNY，其他值列表详见货币类型
-	CashRefundFee *int64 // 现金退款金额，单位为分，只能为整数，详见支付金额
+	DeviceInfo          string // 微信支付分配的终端设备号，与下单一致
+	RefundChannel       string // 退款渠道
+	FeeType             string // 订单金额货币类型，符合ISO 4217标准的三位字母代码，默认人民币：CNY，其他值列表详见货币类型
+	SettlementRefundFee *int64 // 退款金额=申请退款金额-非充值代金券退款金额，退款金额<=申请退款金额
+	SettlementTotalFee  *int64 // 应结订单金额=订单金额-非充值代金券金额，应结订单金额<=订单金额。
+	CashRefundFee       *int64 // 现金退款金额，单位为分，只能为整数，详见支付金额
 
 	// TODO: 增加代金券相关的数据结构
 }
@@ -126,29 +126,33 @@ func Refund2(clt *core.Client, req *RefundRequest) (resp *RefundResponse, err er
 	} else {
 		resp.RefundFee = n
 	}
-	if n, err = strconv.ParseInt(m2["settlement_refund_fee"], 10, 64); err != nil {
-		err = fmt.Errorf("parse settlement_refund_fee:%q to int64 failed: %s", m2["settlement_refund_fee"], err.Error())
-		return
-	} else {
-		resp.SettlementRefundFee = n
-	}
 	if n, err = strconv.ParseInt(m2["total_fee"], 10, 64); err != nil {
 		err = fmt.Errorf("parse total_fee:%q to int64 failed: %s", m2["total_fee"], err.Error())
 		return
 	} else {
 		resp.TotalFee = n
 	}
-	if n, err = strconv.ParseInt(m2["settlement_total_fee"], 10, 64); err != nil {
-		err = fmt.Errorf("parse settlement_total_fee:%q to int64 failed: %s", m2["settlement_total_fee"], err.Error())
-		return
-	} else {
-		resp.SettlementTotalFee = n
-	}
 	if n, err = strconv.ParseInt(m2["cash_fee"], 10, 64); err != nil {
 		err = fmt.Errorf("parse cash_fee:%q to int64 failed: %s", m2["cash_fee"], err.Error())
 		return
 	} else {
 		resp.CashFee = n
+	}
+	if str = m2["settlement_refund_fee"]; str != "" {
+		if n, err = strconv.ParseInt(str, 10, 64); err != nil {
+			err = fmt.Errorf("parse settlement_refund_fee:%q to int64 failed: %s", str, err.Error())
+			return
+		} else {
+			resp.SettlementRefundFee = util.Int64(n)
+		}
+	}
+	if str = m2["settlement_total_fee"]; str != "" {
+		if n, err = strconv.ParseInt(str, 10, 64); err != nil {
+			err = fmt.Errorf("parse settlement_total_fee:%q to int64 failed: %s", str, err.Error())
+			return
+		} else {
+			resp.SettlementTotalFee = util.Int64(n)
+		}
 	}
 	if str = m2["cash_refund_fee"]; str != "" {
 		if n, err = strconv.ParseInt(str, 10, 64); err != nil {
