b813c7f075a2ed931b662e93c3bc83a9513a7244
chan <chanxuehong@gmail.com>
2017-06-10 16:22:24 +0800

    /pay/unifiedorder

--- mch/pay/unifiedorder.go
+++ mch/pay/unifiedorder.go
@@ -3,6 +3,7 @@ package pay
 import (
 	"crypto/md5"
 	"strconv"
+	"time"
 
 	"github.com/chanxuehong/rand"
 
@@ -15,40 +16,40 @@ func UnifiedOrder(clt *core.Client, req map[string]string) (resp map[string]stri
 }
 
 type UnifiedOrderRequest struct {
-	DeviceInfo     string // 终端设备号(门店号或收银设备ID)，注意：PC网页或公众号内支付请传"WEB"
-	NonceStr       string // 随机字符串，不长于32位。NOTE: 如果为空则系统会自动生成一个随机字符串。
-	Body           string // 商品或支付单简要描述
-	Detail         string // 商品名称明细列表
-	Attach         string // 附加数据，在查询API和支付通知中原样返回，该字段主要用于商户携带订单的自定义数据
-	OutTradeNo     string // 商户系统内部的订单号,32个字符内、可包含字母, 其他说明见商户订单号
-	FeeType        string // 符合ISO 4217标准的三位字母代码，默认人民币：CNY，其他值列表详见货币类型
-	TotalFee       int64  // 订单总金额，单位为分，详见支付金额
-	SpbillCreateIP string // APP和网页支付提交用户端ip，Native支付填调用微信支付API的机器IP。
-	TimeStart      string // 订单生成时间，格式为yyyyMMddHHmmss，如2009年12月25日9点10分10秒表示为20091225091010。其他详见时间规则
-	TimeExpire     string // 订单失效时间，格式为yyyyMMddHHmmss，如2009年12月27日9点10分10秒表示为20091227091010。其他详见时间规则
-	GoodsTag       string // 商品标记，代金券或立减优惠功能的参数，说明详见代金券或立减优惠
-	NotifyURL      string // 接收微信支付异步通知回调地址，通知url必须为直接可访问的url，不能携带参数。
-	TradeType      string // 取值如下：JSAPI，NATIVE，APP，详细说明见参数规定
-	ProductId      string // trade_type=NATIVE，此参数必传。此id为二维码中包含的商品ID，商户自行定义。
-	LimitPay       string // no_credit--指定不能使用信用卡支付
-	OpenId         string // rade_type=JSAPI，此参数必传，用户在商户appid下的唯一标识。
+	DeviceInfo     string    `xml:"device_info"`      // 终端设备号(门店号或收银设备ID)，注意：PC网页或公众号内支付请传"WEB"
+	NonceStr       string    `xml:"nonce_str"`        // 随机字符串，不长于32位。NOTE: 如果为空则系统会自动生成一个随机字符串。
+	SignType       string    `xml:"sign_type"`        // 签名类型，默认为MD5，支持HMAC-SHA256和MD5。
+	Body           string    `xml:"body"`             // 商品或支付单简要描述
+	Detail         string    `xml:"detail"`           // 商品名称明细列表
+	Attach         string    `xml:"attach"`           // 附加数据，在查询API和支付通知中原样返回，该字段主要用于商户携带订单的自定义数据
+	OutTradeNo     string    `xml:"out_trade_no"`     // 商户系统内部的订单号,32个字符内、可包含字母, 其他说明见商户订单号
+	FeeType        string    `xml:"fee_type"`         // 符合ISO 4217标准的三位字母代码，默认人民币：CNY，其他值列表详见货币类型
+	TotalFee       int64     `xml:"total_fee"`        // 订单总金额，单位为分，详见支付金额
+	SpbillCreateIP string    `xml:"spbill_create_ip"` // APP和网页支付提交用户端ip，Native支付填调用微信支付API的机器IP。
+	TimeStart      time.Time `xml:"time_start"`       // 订单生成时间，格式为yyyyMMddHHmmss，如2009年12月25日9点10分10秒表示为20091225091010。其他详见时间规则
+	TimeExpire     time.Time `xml:"time_expire"`      // 订单失效时间，格式为yyyyMMddHHmmss，如2009年12月27日9点10分10秒表示为20091227091010。其他详见时间规则
+	GoodsTag       string    `xml:"goods_tag"`        // 商品标记，代金券或立减优惠功能的参数，说明详见代金券或立减优惠
+	NotifyURL      string    `xml:"notify_url"`       // 接收微信支付异步通知回调地址，通知url必须为直接可访问的url，不能携带参数。
+	TradeType      string    `xml:"trade_type"`       // 取值如下：JSAPI，NATIVE，APP，详细说明见参数规定
+	ProductId      string    `xml:"product_id"`       // trade_type=NATIVE，此参数必传。此id为二维码中包含的商品ID，商户自行定义。
+	LimitPay       string    `xml:"limit_pay"`        // no_credit--指定不能使用信用卡支付
+	OpenId         string    `xml:"openid"`           // rade_type=JSAPI，此参数必传，用户在商户appid下的唯一标识。
+	SceneInfo      string    `xml:"scene_info"`       // 该字段用于上报支付的场景信息,针对H5支付有以下三种场景,请根据对应场景上报,H5支付不建议在APP端使用，针对场景1，2请接入APP支付，不然可能会出现兼容性问题
 }
 
 type UnifiedOrderResponse struct {
-	AppId string // 微信开放平台审核通过的应用APPID
-	MchId string // 微信支付分配的商户号
-
-	TradeType string // 调用接口提交的交易类型，取值如下：JSAPI，NATIVE，APP，详细说明见参数规定
-	PrepayId  string // 微信生成的预支付回话标识，用于后续接口调用中使用，该值有效期为2小时
+	TradeType string `xml:"trade_type"` // 调用接口提交的交易类型，取值如下：JSAPI，NATIVE，APP，详细说明见参数规定
+	PrepayId  string `xml:"prepay_id"`  // 微信生成的预支付回话标识，用于后续接口调用中使用，该值有效期为2小时
 
 	// 下面字段都是可选返回的(详细见微信支付文档), 为空值表示没有返回, 程序逻辑里需要判断
-	DeviceInfo string // 调用接口提交的终端设备号。
-	CodeURL    string // trade_type 为 NATIVE 时有返回，可将该参数值生成二维码展示出来进行扫码支付
-	MWebURL    string // trade_type 为 MWEB 时有返回
+	DeviceInfo string `xml:"device_info"` // 调用接口提交的终端设备号。
+	CodeURL    string `xml:"code_url"`    // trade_type 为 NATIVE 时有返回，可将该参数值生成二维码展示出来进行扫码支付
+	MWebURL    string `xml:"mweb_url"`    // trade_type 为 MWEB 时有返回
 }
 
+// 统一下单.
 func UnifiedOrder2(clt *core.Client, req *UnifiedOrderRequest) (resp *UnifiedOrderResponse, err error) {
-	m1 := make(map[string]string, 20)
+	m1 := make(map[string]string, 24)
 	m1["appid"] = clt.AppId()
 	m1["mch_id"] = clt.MchId()
 	if req.DeviceInfo != "" {
@@ -58,6 +59,10 @@ func UnifiedOrder2(clt *core.Client, req *UnifiedOrderRequest) (resp *UnifiedOrd
 		m1["nonce_str"] = req.NonceStr
 	} else {
 		m1["nonce_str"] = string(rand.NewHex())
+	}
+	if req.SignType != "" {
+		// m1["sign_type"] = req.SignType
+		m1["sign_type"] = "MD5" // TODO(chanxuehong): 目前只支持 MD5, 后期修改
 	}
 	m1["body"] = req.Body
 	if req.Detail != "" {
@@ -72,11 +77,11 @@ func UnifiedOrder2(clt *core.Client, req *UnifiedOrderRequest) (resp *UnifiedOrd
 	}
 	m1["total_fee"] = strconv.FormatInt(req.TotalFee, 10)
 	m1["spbill_create_ip"] = req.SpbillCreateIP
-	if req.TimeStart != "" {
-		m1["time_start"] = req.TimeStart
+	if !req.TimeStart.IsZero() {
+		m1["time_start"] = core.FormatTime(req.TimeStart)
 	}
-	if req.TimeExpire != "" {
-		m1["time_expire"] = req.TimeExpire
+	if !req.TimeExpire.IsZero() {
+		m1["time_expire"] = core.FormatTime(req.TimeExpire)
 	}
 	if req.GoodsTag != "" {
 		m1["goods_tag"] = req.GoodsTag
@@ -92,32 +97,17 @@ func UnifiedOrder2(clt *core.Client, req *UnifiedOrderRequest) (resp *UnifiedOrd
 	if req.OpenId != "" {
 		m1["openid"] = req.OpenId
 	}
-	m1["sign"] = core.Sign(m1, clt.ApiKey(), md5.New)
+	if req.SceneInfo != "" {
+		m1["scene_info"] = req.SceneInfo
+	}
+	m1["sign"] = core.Sign(m1, clt.ApiKey(), md5.New) // TODO(chanxuehong): 目前只支持 MD5, 后期修改
 
 	m2, err := UnifiedOrder(clt, m1)
 	if err != nil {
 		return
 	}
 
-	// 判断业务状态
-	resultCode, ok := m2["result_code"]
-	if !ok {
-		err = core.ErrNotFoundResultCode
-		return
-	}
-	if resultCode != core.ResultCodeSuccess {
-		err = &core.BizError{
-			ResultCode:  resultCode,
-			ErrCode:     m2["err_code"],
-			ErrCodeDesc: m2["err_code_des"],
-		}
-		return
-	}
-
 	resp = &UnifiedOrderResponse{
-		AppId: m2["appid"],
-		MchId: m2["mch_id"],
-
 		TradeType:  m2["trade_type"],
 		PrepayId:   m2["prepay_id"],
 		DeviceInfo: m2["device_info"],
