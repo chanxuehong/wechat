aed8995ef0efd26acbd9d9dfdc40c2052af3550f
chan <chanxuehong@gmail.com>
2017-06-17 21:33:49 +0800

    ssl client

--- mch/core/ssl_http_client_go1.7.go
+++ mch/core/ssl_http_client_go1.7.go
@@ -35,3 +35,30 @@ func NewTLSHttpClient(certFile, keyFile string) (httpClient *http.Client, err er
 		Timeout: 5 * time.Second,
 	}, nil
 }
+
+// NewTLSHttpClient2 创建支持双向证书认证的 http.Client.
+func NewTLSHttpClient2(certPEMBlock, keyPEMBlock []byte) (httpClient *http.Client, err error) {
+	cert, err := tls.X509KeyPair(certPEMBlock, keyPEMBlock)
+	if err != nil {
+		return nil, err
+	}
+	tlsConfig := &tls.Config{
+		Certificates: []tls.Certificate{cert},
+	}
+
+	return &http.Client{
+		Transport: &http.Transport{
+			Proxy: http.ProxyFromEnvironment,
+			DialContext: (&net.Dialer{
+				Timeout:   3 * time.Second,
+				KeepAlive: 30 * time.Second,
+			}).DialContext,
+			MaxIdleConns:          100,
+			IdleConnTimeout:       90 * time.Second,
+			TLSHandshakeTimeout:   3 * time.Second,
+			ExpectContinueTimeout: 1 * time.Second,
+			TLSClientConfig:       tlsConfig,
+		},
+		Timeout: 5 * time.Second,
+	}, nil
+}
