daeda374fb99f960979c9ed1b6fcaf5dc8a5ae31
chanxuehong <chanxuehong@qiniu.com>
2017-09-11 15:23:53 +0800

    CreateStrSceneTempQrcode

--- mp/qrcode/create.go
+++ mp/qrcode/create.go
@@ -48,6 +48,40 @@ func CreateTempQrcode(clt *core.Client, sceneId int32, expireSeconds int) (qrcod
 	return
 }
 
+// CreateStrSceneTempQrcode 创建临时二维码.
+//  sceneStr:      场景值ID(字符串形式的ID), 字符串类型, 长度限制为1到64
+//  expireSeconds: 二维码有效时间, 以秒为单位
+func CreateStrSceneTempQrcode(clt *core.Client, sceneStr string, expireSeconds int) (qrcode *TempQrcode, err error) {
+	const incompleteURL = "https://api.weixin.qq.com/cgi-bin/qrcode/create?access_token="
+
+	var request struct {
+		ExpireSeconds int    `json:"expire_seconds"`
+		ActionName    string `json:"action_name"`
+		ActionInfo    struct {
+			Scene struct {
+				SceneStr string `json:"scene_str"`
+			} `json:"scene"`
+		} `json:"action_info"`
+	}
+	request.ExpireSeconds = expireSeconds
+	request.ActionName = "QR_STR_SCENE"
+	request.ActionInfo.Scene.SceneStr = sceneStr
+
+	var result struct {
+		core.Error
+		TempQrcode
+	}
+	if err = clt.PostJSON(incompleteURL, &request, &result); err != nil {
+		return
+	}
+	if result.ErrCode != core.ErrCodeOK {
+		err = &result.Error
+		return
+	}
+	qrcode = &result.TempQrcode
+	return
+}
+
 // CreatePermQrcode 创建永久二维码
 //  sceneId: 场景值ID
 func CreatePermQrcode(clt *core.Client, sceneId int32) (qrcode *PermQrcode, err error) {
