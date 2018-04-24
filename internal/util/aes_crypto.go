5dc47eae83b599e93de6780e30e579a8d64c872d
zhouchao <zhouc@ttouch.com.cn>
2018-04-24 23:10:34 +0800

    add sessionKey 小程序

--- internal/util/aes_crypto.go
+++ internal/util/aes_crypto.go
@@ -115,3 +115,46 @@ func AESDecryptMsg(ciphertext []byte, aesKey []byte) (random, rawXMLMsg, appId [
 	appId = plaintext[appIdOffset:]
 	return
 }
+
+func AESDecryptData(cipherText []byte, aesKey []byte, iv []byte)  (rawData []byte, err error) {
+
+	const (
+		BLOCK_SIZE = 32             // PKCS#7
+		BLOCK_MASK = BLOCK_SIZE - 1 // BLOCK_SIZE 为 2^n 时, 可以用 mask 获取针对 BLOCK_SIZE 的余数
+	)
+
+	if len(cipherText) < BLOCK_SIZE {
+		err = fmt.Errorf("the length of ciphertext too short: %d", len(cipherText))
+		return
+	}
+
+	plaintext := make([]byte, len(cipherText)) // len(plaintext) >= BLOCK_SIZE
+
+	// 解密
+	block, err := aes.NewCipher(aesKey)
+	if err != nil {
+		panic(err)
+	}
+	mode := cipher.NewCBCDecrypter(block, iv)
+	mode.CryptBlocks(plaintext, cipherText)
+
+	// PKCS#7 去除补位
+	amountToPad := int(plaintext[len(plaintext)-1])
+	if amountToPad < 1 || amountToPad > BLOCK_SIZE {
+		err = fmt.Errorf("the amount to pad is incorrect: %d", amountToPad)
+		return
+	}
+	plaintext = plaintext[:len(plaintext)-amountToPad]
+
+	// 反拼接
+	// len(plaintext) == 16+4+len(rawXMLMsg)+len(appId)
+	if len(plaintext) <= 20 {
+		err = fmt.Errorf("plaintext too short, the length is %d", len(plaintext))
+		return
+	}
+
+	rawData = plaintext
+
+	return
+
+}
---
+++ mp/oauth2/api_test.go
@@ -0,0 +1,37 @@
+package oauth2
+
+import (
+	"fmt"
+	"testing"
+)
+
+const (
+	wxAppId     = "wxa33cba2b69f869f3"               // 填上自己的参数
+	wxAppSecret = "ca202c5592c3bebd9f9dfa1ee9e39847" // 填上自己的参数
+	//oauth2RedirectURI = "http://192.168.1.129:8080/page2" // 填上自己的参数
+	//oauth2Scope       = "snsapi_userinfo"                 // 填上自己的参数
+)
+
+var (
+	oauth2Endpoint *Endpoint = NewEndpoint(wxAppId, wxAppSecret)
+)
+
+func TestGetSession(t *testing.T) {
+
+	GetSession(oauth2Endpoint, "013Rc7FP0lmgxb2lRIIP0VefFP0Rc7FW")
+}
+
+func TestGetUserInfoBySession(t *testing.T) {
+
+	session := Session{SessionKey: "HogNecGqZeDxFIDGjBwWKw=="}
+
+	iv := "aoZqkfGDWwqj6rFgWdafyw=="
+
+	encrypt := "4B9B1aknFM6yQjAh9mFxH3iN4PZXUGfpBLB98CRAVZzNfUI4J1WUur70+NSH/5MXcCidrt44hi6dkByTtRPxrTIV1BOOTvaa2G5NWunTXZJ37/Oq0ezydbDal5v+X3bvVVeFR6MkhYI+hT9xVl2XnE/Bzon2gIq9F9Fy8Yny0VPqsQ95xUyXnN3/IuhiquR1pAgKjDK3kgCoqhUVNa0dRRQQgTNIpy1djbLfyErPXGTXe1qhAj7RvDdJtRloEfg63JgaB/QTR2BLEGT7/GfHnwROfngxa3esGDeBr9Mtav67R4PjYESVFtH2Yf2npaDWAvAMvr+8hNVd2tjy/3HgImctZWo7bh1OHa/ktH4wKYVbTgxZPg/lgTWC+zsl1Z9g07vWQyGpaA11HODDGn1Kdvh6esiY7T3JQ15nKs1rdyXigNQFb9+kicPiInKVfOiuMcGEazli5/UQHF5rnuWhkg/wy+PRbZybR18lLh5d+EW1CnK8gNcQblDJPvx6QFcFhPxr28WkEd7ys0BZQGCIkQ=="
+
+	info, err := GetSessionInfo(encrypt, session, iv)
+
+	fmt.Println(info)
+
+	fmt.Println(err)
+}
