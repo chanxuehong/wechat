b539865c454c4b488136e960bc48320918a7167c
Syd Xu <prof.syd.xu@gmail.com>
2019-08-26 11:36:17 +0800

    fix AuthorizerInfo struct

--- component/authorizerInfo.go
+++ component/authorizerInfo.go
@@ -1,15 +1,19 @@
 package component
 
 type AuthorizerInfo struct {
-	NickName        string `json:"nick_name"`         // 授权方昵称
-	HeadImg         string `json:"head_img"`          // 授权方头像
-	ServiceTypeInfo uint   `json:"service_type_info"` // 授权方公众号类型，0代表订阅号，1代表由历史老帐号升级后的订阅号，2代表服务号
-	VerifyTypeInfo  int    `json:"verify_type_info"`  // 授权方认证类型，-1代表未认证，0代表微信认证，1代表新浪微博认证，2代表腾讯微博认证，3代表已资质认证通过但还未通过名称认证，4代表已资质认证通过、还未通过名称认证，但通过了新浪微博认证，5代表已资质认证通过、还未通过名称认证，但通过了腾讯微博认证
-	UserName        string `json:"user_name"`         // 授权方公众号的原始ID
-	Signature       string `json:"signature"`         // 帐号介绍
-	PrincipalName   string `json:"principal_name"`    // 公众号的主体名称
-	Alias           string `json:"alias,omitempty"`   // 授权方公众号所设置的微信号，可能为空
-	BusinessInfo    *struct {
+	NickName        string `json:"nick_name"` // 授权方昵称
+	HeadImg         string `json:"head_img"`  // 授权方头像
+	ServiceTypeInfo *struct {
+		Id uint `json:"id"`
+	} `json:"service_type_info"` // 授权方公众号类型，0代表订阅号，1代表由历史老帐号升级后的订阅号，2代表服务号
+	VerifyTypeInfo *struct {
+		Id int `json:"id"`
+	} `json:"verify_type_info"` // 授权方认证类型，-1代表未认证，0代表微信认证，1代表新浪微博认证，2代表腾讯微博认证，3代表已资质认证通过但还未通过名称认证，4代表已资质认证通过、还未通过名称认证，但通过了新浪微博认证，5代表已资质认证通过、还未通过名称认证，但通过了腾讯微博认证
+	UserName      string `json:"user_name"`       // 授权方公众号的原始ID
+	Signature     string `json:"signature"`       // 帐号介绍
+	PrincipalName string `json:"principal_name"`  // 公众号的主体名称
+	Alias         string `json:"alias,omitempty"` // 授权方公众号所设置的微信号，可能为空
+	BusinessInfo  *struct {
 		OpenStore uint `json:"open_store"` // 是否开通微信门店功能
 		OpenScan  uint `json:"open_scan"`  // 是否开通微信扫商品功能
 		OpenPay   uint `json:"open_pay"`   // 是否开通微信支付功能
