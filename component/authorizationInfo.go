d4f6b9cca42ddcd5d5c5424fb5998937a86a9a43
Syd Xu <prof.syd.xu@gmail.com>
2019-09-25 11:13:34 +0800

    omitempty

--- component/authorizationInfo.go
+++ component/authorizationInfo.go
@@ -1,20 +1,20 @@
 package component
 
 type AuthorizationInfo struct {
-	AuthorizerAppId        string     `json:"authorizer_appid"`         // 授权方appid
-	AuthorizerAccessToken  string     `json:"authorizer_access_token"`  // 授权方接口调用凭据（在授权的公众号或小程序具备API权限时，才有此返回值），也简称为令牌
-	ExpiresIn              int64      `json:"expires_in"`               // 有效期（在授权的公众号或小程序具备API权限时，才有此返回值）
-	AuthorizerRefreshToken string     `json:"authorizer_refresh_token"` // 接口调用凭据刷新令牌（在授权的公众号具备API权限时，才有此返回值），刷新令牌主要用于第三方平台获取和刷新已授权用户的access_token，只会在授权时刻提供，请妥善保存。 一旦丢失，只能让用户重新授权，才能再次拿到新的刷新令牌
-	FuncInfo               []FuncInfo `json:"func_info"`                // 授权给开发者的权限集列表，ID为1到26分别代表： 1、消息管理权限 2、用户管理权限 3、帐号服务权限 4、网页服务权限 5、微信小店权限 6、微信多客服权限 7、群发与通知权限 8、微信卡券权限 9、微信扫一扫权限 10、微信连WIFI权限 11、素材管理权限 12、微信摇周边权限 13、微信门店权限 15、自定义菜单权限 16、获取认证状态及信息 17、帐号管理权限（小程序） 18、开发管理与数据分析权限（小程序） 19、客服消息管理权限（小程序） 20、微信登录权限（小程序） 21、数据分析权限（小程序） 22、城市服务接口权限 23、广告管理权限 24、开放平台帐号管理权限 25、 开放平台帐号管理权限（小程序） 26、微信电子发票权限 41、搜索widget的权限 请注意： 1）该字段的返回不会考虑公众号是否具备该权限集的权限（因为可能部分具备），请根据公众号的帐号类型和认证情况，来判断公众号的接口权限。
+	AuthorizerAppId        string     `json:"authorizer_appid,omitempty"`         // 授权方appid
+	AuthorizerAccessToken  string     `json:"authorizer_access_token,omitempty"`  // 授权方接口调用凭据（在授权的公众号或小程序具备API权限时，才有此返回值），也简称为令牌
+	ExpiresIn              int64      `json:"expires_in,omitempty"`               // 有效期（在授权的公众号或小程序具备API权限时，才有此返回值）
+	AuthorizerRefreshToken string     `json:"authorizer_refresh_token,omitempty"` // 接口调用凭据刷新令牌（在授权的公众号具备API权限时，才有此返回值），刷新令牌主要用于第三方平台获取和刷新已授权用户的access_token，只会在授权时刻提供，请妥善保存。 一旦丢失，只能让用户重新授权，才能再次拿到新的刷新令牌
+	FuncInfo               []FuncInfo `json:"func_info,omitempty"`                // 授权给开发者的权限集列表，ID为1到26分别代表： 1、消息管理权限 2、用户管理权限 3、帐号服务权限 4、网页服务权限 5、微信小店权限 6、微信多客服权限 7、群发与通知权限 8、微信卡券权限 9、微信扫一扫权限 10、微信连WIFI权限 11、素材管理权限 12、微信摇周边权限 13、微信门店权限 15、自定义菜单权限 16、获取认证状态及信息 17、帐号管理权限（小程序） 18、开发管理与数据分析权限（小程序） 19、客服消息管理权限（小程序） 20、微信登录权限（小程序） 21、数据分析权限（小程序） 22、城市服务接口权限 23、广告管理权限 24、开放平台帐号管理权限 25、 开放平台帐号管理权限（小程序） 26、微信电子发票权限 41、搜索widget的权限 请注意： 1）该字段的返回不会考虑公众号是否具备该权限集的权限（因为可能部分具备），请根据公众号的帐号类型和认证情况，来判断公众号的接口权限。
 }
 
 type FuncInfo struct {
 	FuncScopeCategory *struct {
 		Id uint `json:"id"`
-	} `json:"funcscope_category"`
+	} `json:"funcscope_category,omitempty"`
 	ConfirmInfo *struct {
-		Need   uint `json:"need_confirm"`
-		Aleady uint `json:"already_confirm"`
-		Can    uint `json:"can_confirm"`
+		Need   uint `json:"need_confirm,omitempty"`
+		Aleady uint `json:"already_confirm,omitempty"`
+		Can    uint `json:"can_confirm,omitempty"`
 	} `json:"confirm_info,omitempty"`
 }
--- component/authorizerInfo.go
+++ component/authorizerInfo.go
@@ -1,22 +1,22 @@
 package component
 
 type AuthorizerInfo struct {
-	NickName        string `json:"nick_name"` // 授权方昵称
-	HeadImg         string `json:"head_img"`  // 授权方头像
+	NickName        string `json:"nick_name,omitempty"` // 授权方昵称
+	HeadImg         string `json:"head_img,omitempty"`  // 授权方头像
 	ServiceTypeInfo *struct {
 		Id uint `json:"id"`
-	} `json:"service_type_info"` // 授权方公众号类型，0代表订阅号，1代表由历史老帐号升级后的订阅号，2代表服务号
+	} `json:"service_type_info,omitempty"` // 授权方公众号类型，0代表订阅号，1代表由历史老帐号升级后的订阅号，2代表服务号
 	VerifyTypeInfo *struct {
 		Id int `json:"id"`
-	} `json:"verify_type_info"` // 授权方认证类型，-1代表未认证，0代表微信认证，1代表新浪微博认证，2代表腾讯微博认证，3代表已资质认证通过但还未通过名称认证，4代表已资质认证通过、还未通过名称认证，但通过了新浪微博认证，5代表已资质认证通过、还未通过名称认证，但通过了腾讯微博认证
-	UserName          string             `json:"user_name"`          // 授权方公众号的原始ID
-	Signature         string             `json:"signature"`          // 帐号介绍
-	PrincipalName     string             `json:"principal_name"`     // 公众号的主体名称
-	Alias             string             `json:"alias,omitempty"`    // 授权方公众号所设置的微信号，可能为空
-	BusinessInfo      *BusinessInfo      `json:"business_info"`      // 用以了解以下功能的开通状况（0代表未开通，1代表已开通）
-	QrcodeUrl         string             `json:"qrcode_url"`         // 二维码图片的URL，开发者最好自行也进行保存
-	MiniProgramInfo   *MiniProgramInfo   `json:"MiniProgramInfo"`    // 可根据这个字段判断是否为小程序类型授权
-	AuthorizationInfo *AuthorizationInfo `json:"authorization_info"` // 授权信息
+	} `json:"verify_type_info,omitempty"` // 授权方认证类型，-1代表未认证，0代表微信认证，1代表新浪微博认证，2代表腾讯微博认证，3代表已资质认证通过但还未通过名称认证，4代表已资质认证通过、还未通过名称认证，但通过了新浪微博认证，5代表已资质认证通过、还未通过名称认证，但通过了腾讯微博认证
+	UserName          string             `json:"user_name,omitempty"`          // 授权方公众号的原始ID
+	Signature         string             `json:"signature,omitempty"`          // 帐号介绍
+	PrincipalName     string             `json:"principal_name,omitempty"`     // 公众号的主体名称
+	Alias             string             `json:"alias,omitempty"`              // 授权方公众号所设置的微信号，可能为空
+	BusinessInfo      *BusinessInfo      `json:"business_info,omitempty"`      // 用以了解以下功能的开通状况（0代表未开通，1代表已开通）
+	QrcodeUrl         string             `json:"qrcode_url,omitempty"`         // 二维码图片的URL，开发者最好自行也进行保存
+	MiniProgramInfo   *MiniProgramInfo   `json:"MiniProgramInfo,omitempty"`    // 可根据这个字段判断是否为小程序类型授权
+	AuthorizationInfo *AuthorizationInfo `json:"authorization_info,omitempty"` // 授权信息
 }
 
 type BusinessInfo struct {
@@ -28,9 +28,9 @@ type BusinessInfo struct {
 }
 
 type MiniProgramInfo struct {
-	Network     *MiniProgramNetwork `json:"network"`              // 小程序已设置的各个服务器域名
+	Network     *MiniProgramNetwork `json:"network,omitempty"`    // 小程序已设置的各个服务器域名
 	Categories  []map[string]string `json:"categories,omitempty"` //
-	VisitStatus uint                `json:"visit_status"`
+	VisitStatus uint                `json:"visit_status,omitempty"`
 }
 
 type MiniProgramNetwork struct {
