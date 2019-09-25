abb5fa90675707127cfa66e8e8aef08e4e63c6db
Syd Xu <prof.syd.xu@gmail.com>
2019-09-25 18:45:57 +0800

    Update release.go

--- miniapp/wxa/release.go
+++ miniapp/wxa/release.go
@@ -5,7 +5,7 @@ import (
 )
 
 // 发布已通过审核的小程序（仅供第三方代小程序调用）
-func Release(clt *core.Client, req *ModifyDomainRequest) (err error) {
+func Release(clt *core.Client) (err error) {
 	const incompleteURL = "https://api.weixin.qq.com/wxa/release?access_token="
 	var result struct {
 		core.Error
