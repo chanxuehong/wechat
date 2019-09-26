19e2a45c9b120cb80a8f5fa78a04a93b96aec121
Syd Xu <prof.syd.xu@gmail.com>
2019-09-26 13:09:49 +0800

    Update queryQuota.go

--- miniapp/wxa/queryQuota.go
+++ miniapp/wxa/queryQuota.go
@@ -12,11 +12,11 @@ type Quota struct {
 }
 
 // 查询服务商的当月提审限额（quota）和加急次数
-func QueryQuota(clt *core.Client) (*quota Quota, err error) {
+func QueryQuota(clt *core.Client) (quota *Quota, err error) {
 	const incompleteURL = "https://api.weixin.qq.com/wxa/queryquota?access_token="
 	var result struct {
 		core.Error
-        Quota
+		Quota
 	}
 	req := map[string]interface{}{}
 	if err = clt.PostJSON(incompleteURL, req, &result); err != nil {
@@ -26,10 +26,10 @@ func QueryQuota(clt *core.Client) (*quota Quota, err error) {
 		err = &result.Error
 		return nil, err
 	}
-	return &Quota {
-        Rest: result.Rest,
-        Limit: result.Limit,
-        SpeedupRest: result.SpeedupRest,
-        SpeedupLimit: result.SpeedupLimit,
-    }, nil
+	return &Quota{
+		Rest:         result.Rest,
+		Limit:        result.Limit,
+		SpeedupRest:  result.SpeedupRest,
+		SpeedupLimit: result.SpeedupLimit,
+	}, nil
 }
