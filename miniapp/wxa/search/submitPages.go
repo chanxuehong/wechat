65b42b12d0a7f1b841176ccb85e0c91ff103b004
Syd Xu <prof.syd.xu@gmail.com>
2020-02-01 18:40:29 +0800

    add wxa/search/submitPages api

--- miniapp/wxa/search/submitPages.go
+++ miniapp/wxa/search/submitPages.go
@@ -15,7 +15,7 @@ type Page struct {
 
 // 小程序开发者可以通过本接口提交小程序页面url及参数信息，让微信可以更及时的收录到小程序的页面信息，开发者提交的页面信息将可能被用于小程序搜索结果展示。
 func SubmitPages(clt *core.Client, req *SubmitPagesRequest) error {
-	const incompleteURL = "api.weixin.qq.com/wxa/search/wxaapi_submitpages?access_token="
+	const incompleteURL = "https://api.weixin.qq.com/wxa/search/wxaapi_submitpages?access_token="
 	var result struct {
 		core.Error
 	}
