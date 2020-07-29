e669d37787118d343684f5037e9afa34afb385c8
Syd Xu <prof.syd.xu@gmail.com>
2020-07-29 14:02:26 +0800

    fix externalcontact getCorpTagList api

--- work/externalcontact/getCorpTagList.go
+++ work/externalcontact/getCorpTagList.go
@@ -6,12 +6,12 @@ import (
 
 // GetCorpTagList 获取企业标签库
 // tag_id: 要查询的标签id，如果不填则获取该企业的所有客户标签，目前暂不支持标签组id
-func GetCorpTagList(clt *core.Client, tagId string) (rslt []TagGroup, err error) {
-	const incompleteURL = "https://qyapi.weixin.qq.com/cgi-bin/externalcontact/groupchat/get?access_token="
+func GetCorpTagList(clt *core.Client, tagIds []string) (rslt []TagGroup, err error) {
+	const incompleteURL = "https://qyapi.weixin.qq.com/cgi-bin/externalcontact/get_corp_tag_list?access_token="
 
-	request := make(map[string]string, 1)
-	if tagId != "" {
-		request["tag_id"] = tagId
+	request := make(map[string][]string, 1)
+	if len(tagIds) > 0 {
+		request["tag_id"] = tagIds
 	}
 	var result struct {
 		core.Error
