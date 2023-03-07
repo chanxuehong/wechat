44214cdf7a9a564a84d2af75df4ab3da62b5d9ef
Syd Xu <prof.syd.xu@gmail.com>
2023-03-07 12:52:02 +0800

    fix(minapp): message related structure

--- miniapp/message/subscribe/message.go
+++ miniapp/message/subscribe/message.go
@@ -5,8 +5,16 @@ import (
 )
 
 type Message struct {
-	ToUser     string                       `json:"touser"`         // 必须, 接受者OpenID
-	TemplateId string                       `json:"template_id"`    // 必须, 模版ID
-	Page       string                       `json:"page,omitempty"` // 点击模板卡片后的跳转页面，仅限本小程序内的页面。支持带参数,（示例index?foo=bar）。该字段不填则模板无跳转。formId；支付场景下，为本次支付的 prepay_id
-	Data       map[string]template.DataItem `json:"data"`           // 模板内容，不填则下发空模板。具体格式请参考示例。
+	// ToUser 必须, 接受者OpenID
+	ToUser string `json:"touser"`
+	// TemplateId 必须, 模版ID
+	TemplateId string `json:"template_id"`
+	// Page 点击模板卡片后的跳转页面，仅限本小程序内的页面。支持带参数,（示例index?foo=bar）。该字段不填则模板无跳转
+	Page string `json:"page,omitempty"`
+	// MiniProgramState 跳转小程序类型：developer为开发版；trial为体验版；formal为正式版；默认为正式版
+	MiniprogramState string `json:"miniprogram_state,omitempty"`
+	// Date 模板内容，不填则下发空模板。具体格式请参考示例。
+	Data map[string]template.DataItem `json:"data"`
+	// Lang 进入小程序查看”的语言类型，支持zh_CN(简体中文)、en_US(英文)、zh_HK(繁体中文)、zh_TW(繁体中文)，默认为zh_CN
+	Lang string `json:"lang,omitempty"`
 }
